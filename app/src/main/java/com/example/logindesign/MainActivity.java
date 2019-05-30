cc88d1f89770205f8443e9ec7caf3a994cfe0f3f
[deepu khadka] <[deepukhadka2@gmail.com]>
2019-05-29 21:54:25 -0700

    Initial commit

---
+++ app/src/main/java/com/example/logindesign/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.logindesign;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+    private EditText etusername, etpassword;
+    private Button btnlogin;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        etusername = findViewById(R.id.etUsername);
+        etpassword = findViewById(R.id.etPassword);
+        btnlogin = findViewById(R.id.btnlogin);
+
+        btnlogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                
+            
+            login();
+                
+            }
+
+            private void login() {
+            }
+        });
+    }
+}
